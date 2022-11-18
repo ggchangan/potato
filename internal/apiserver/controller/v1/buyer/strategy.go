f301a9a5374097d50bb8a7443edd03eef7385fd9
renzhang <renzhang@freewheel.tv>
2022-11-18 16:01:39 +0800

    init

---
+++ internal/apiserver/controller/v1/buyer/strategy.go
@@ -0,0 +1,117 @@
+package buyer
+
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/marmotedu/iam/pkg/log"
+
+	"github.com/ggchangan/yugong/internal/pkg/util/core"
+)
+
+// Strategy get a strategy to buy a new stock.
+func (r *BuyerController) Strategy(c *gin.Context) {
+	log.L(c).Info("get buyer strategy function called.")
+	//price := c.GetFloat64("price")
+
+	id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
+	user, err := r.srv.Stocks().Get(c, uint64(id))
+	if err != nil {
+		core.WriteResponse(c, err, nil)
+
+		return
+	}
+
+	core.WriteResponse(c, nil, user)
+}
+
+type buyerAlgo interface {
+	Calculate(p Parameter) Result
+}
+
+const DefaultInitialMoney = 100000.00
+const DefaultNormalInterval = 0.1
+const DefaultSpecialInterval = 0.15
+const DefaultSpecialMoney = 50000.00
+
+type Parameter struct {
+	//初始买入价格
+	Price float32
+	//初始投入资金
+	Money float32
+	//前3次买入之后补仓金额
+	SpecialMoney    float32
+	NormalInterval  float32
+	SpecialInterval float32
+}
+
+func NewParameter(money, normalInterval, specialInterval float32) Parameter {
+	return Parameter{
+		Money:           money,
+		NormalInterval:  normalInterval,
+		SpecialInterval: specialInterval,
+	}
+}
+
+type Result struct {
+	Price          []float32
+	Money          []float32
+	Loss           []float32
+	LossPercentage []float32
+}
+
+// 343
+type Default struct {
+	Percentage []float32
+}
+
+// 343
+var DefaultAlgo = func(p Parameter) Result {
+
+	price := make([]float32, 4, 4)
+	price[0] = p.Price
+	for i := 1; i < 3; i++ {
+		price[i] = price[i-1] * (1 - p.NormalInterval)
+	}
+	price[3] = price[2] * (1 - p.SpecialInterval)
+
+	percentage := []float32{0.3, 0.4, 0.3}
+	increment := make([]float32, 4, 4)
+	for i := 0; i < 3; i++ {
+		increment[i] = percentage[i] * p.Money
+	}
+	increment[3] = p.SpecialMoney
+
+	money := make([]float32, 4, 4)
+	money[0] = increment[0]
+	for i := 1; i < 4; i++ {
+		money[i] = money[i-1] + increment[i]
+	}
+
+	interval := []float32{p.NormalInterval, p.NormalInterval, p.SpecialInterval}
+	//亏损额
+	loss := make([]float32, 4, 4)
+	//新一次的亏损 = 之前的亏损 + (已经投入的钱-之前的亏损) * interval
+	loss[0] = 0
+	for i := 1; i < 4; i++ {
+		loss[i] = loss[i-1] + (money[i-1]-loss[i-1])*interval[i-1]
+	}
+
+	//亏损比率
+	lossPercentage := make([]float32, 4, 4)
+	//亏损的钱，投入的钱
+	for i := 0; i < 4; i++ {
+		lossPercentage[i] = loss[i] / money[i]
+	}
+
+	return Result{
+		Price:          price,
+		Money:          money,
+		Loss:           loss,
+		LossPercentage: lossPercentage,
+	}
+}
+
+type Pyramid struct {
+	Percentage []float32
+}
---
+++ internal/apiserver/router.go
@@ -0,0 +1,59 @@
+package apiserver
+
+import (
+	"github.com/ggchangan/yugong/internal/apiserver/controller/v1/buyer"
+	"github.com/ggchangan/yugong/internal/apiserver/controller/v1/stock"
+	"github.com/gin-gonic/gin"
+
+	"github.com/ggchangan/yugong/internal/apiserver/controller/v1/message"
+	"github.com/ggchangan/yugong/internal/apiserver/controller/v1/report"
+	"github.com/ggchangan/yugong/internal/apiserver/store/mysql"
+)
+
+func initRouter(g *gin.Engine) {
+	installMiddleware(g)
+	installController(g)
+}
+
+func installMiddleware(g *gin.Engine) {
+}
+
+func installController(g *gin.Engine) *gin.Engine {
+	// v1 handlers, requiring authentication
+	storeIns, _ := mysql.GetMySQLFactoryOr(nil)
+	v1 := g.Group("/v1")
+	{
+		// report RESTful resource
+		reportv1 := v1.Group("/reports")
+		{
+			reportController := report.NewReportController(storeIns)
+
+			reportv1.POST("", reportController.Create)
+			reportv1.DELETE(":id", reportController.Delete) // admin api
+			reportv1.GET(":id", reportController.Get)       // admin api
+		}
+
+		// report RESTful resource
+		reportMessagev1 := v1.Group("/report_messages")
+		{
+			reportMessageController := message.NewReportMessageController(storeIns)
+
+			reportMessagev1.GET(":id", reportMessageController.Get) // admin api
+		}
+
+		stockv1 := v1.Group("/stock")
+		{
+			stockController := stock.NewStockController(storeIns)
+
+			stockv1.GET(":id", stockController.Get)
+		}
+
+		buyerv1 := v1.Group("/buyer")
+		{
+			buyerController := buyer.NewBuyerController(storeIns)
+			buyerv1.GET("/price", buyerController.Strategy)
+		}
+	}
+
+	return g
+}
